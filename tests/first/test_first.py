25fe2f017059a9826194bd71bc5f96bc5e96d3b2
miles <miles@moda.gov.tw>
2023-12-12 01:10:03 +0000

    QQ

--- tests/first/test_first.py
+++ tests/first/test_first.py
@@ -1,4 +1,11 @@
 import numpy as np
 import pandas
 
-print('Hello World!!')
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "hello")
